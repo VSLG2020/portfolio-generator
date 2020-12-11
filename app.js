357dd0a879e0bb0f76586fa6da13768607aa2714
Vanessa G <vlopez_jv@hotmail.com>
2020-12-10 17:26:39 -0800

    created html, added new page template, and interloped files.

--- app.js
+++ app.js
@@ -40,6 +40,7 @@
 //   }
 // };
 
+// const profileDataArgs = process.argv.slice(2);
 
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -56,6 +57,100 @@
 //   };
 
 
-//Or arrows
+// // Or arrows
 
 // profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+// console.log(generatePage());
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// console.log(generatePage('Jane', 'janehub'));
+
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//       Name: ${userName}
+//       GitHub: ${githubName}
+//     `;
+//   };
+
+// //   console.log(generatePage('Jane', 'janehub'));
+
+
+// //   destructing assignment //
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+// console.log(generatePage(name, github));
+
+// // CREATING HTML//
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+// const [name, github] = profileDataArgs;
+
+
+
+// const generatePage = (name, github) => {
+//     return `
+//     <!DOCTYPE html> 
+//     <html lang="en"> 
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <title>Portfolio Demo</title>
+//     </head>
+  
+//     <body>
+//       <h1>${name}</h1>
+//       <h2><a href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//     `;
+//   };
+
+
+
+// // console.log(name, github);
+// // console.log(generatePage(name, github));
+
+// const fs = require('fs');
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+  
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+//   });
+
+// //   Review the three arguments in the fs.writeFile() function. 
+// // The first argument is the file name that will be created, or the output file. 
+// // The second argument is the data that's being written: the HTML string template. 
+// // The third argument is the callback function that will handle any errors as well as the success message.
+
+//NEW CLEANED UP CODE FROM STUDY GUIDE//
+
+const fs = require('fs');
+
+//added statment to link pages (interlope)
+const generatePage = require('./src/page-template.js');
+
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+//generate page function was cut and moved to page-template.js//
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
